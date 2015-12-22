cb24b4d8c4fcbd5184053dd1331791f6b19d0888
tihmstar <tihmstar@gmail.com>
2015-12-22 09:26:55 +0100

    Initial Commit

---
+++ tsschecker/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tsschecker
+//
+//  Created by tihmstar on 22.12.15.
+//  Copyright © 2015 tihmstar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
