c669bf0f5165b78ee6073b222aded44856fb4c34
tihmstar <tihmstar@gmail.com>
2016-12-21 22:12:29 +0100

    sed -i has different meaning on linux and macos, trying both to set version

--- setBuildVersion.sh
+++ setBuildVersion.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sed -i '.bak' "s/.*VERSION_COMMIT_COUNT.*/#define VERSION_COMMIT_COUNT \"$(git rev-list --count master)\"/" ./tsschecker/all_tsschecker.h 
-sed -i '.bak' "s/.*VERSION_COMMIT_SHA.*/#define VERSION_COMMIT_SHA \"$(git rev-parse HEAD)\"/" ./tsschecker/all_tsschecker.h 
-
+sed -i '.bak' "s/.*VERSION_COMMIT_COUNT.*/#define VERSION_COMMIT_COUNT \"$(git rev-list --count master)\"/" ./tsschecker/all_tsschecker.h 2>/dev/null || sed -i "s/.*VERSION_COMMIT_COUNT.*/#define VERSION_COMMIT_COUNT \"$(git rev-list --count master)\"/" ./tsschecker/all_tsschecker.h 2>/dev/null
+sed -i '.bak' "s/.*VERSION_COMMIT_SHA.*/#define VERSION_COMMIT_SHA \"$(git rev-parse HEAD)\"/" ./tsschecker/all_tsschecker.h 2>/dev/null || sed -i "s/.*VERSION_COMMIT_SHA.*/#define VERSION_COMMIT_SHA \"$(git rev-parse HEAD)\"/" ./tsschecker/all_tsschecker.h 2>/dev/null
