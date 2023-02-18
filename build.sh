3c18c0b1b2670fe140f4d83b17f46cd543d1237b
Cryptiiiic <liamwqs@gmail.com>
2023-02-17 20:54:54 -0800

    Rewrite cmake

--- build.sh
+++ build.sh
@@ -8,21 +8,21 @@ export CXX="$(which clang++)"
 which xcrun >/dev/null
  if [[ "$?" -lt 1 ]]; then export CC="$(xcrun --find clang)"; export CXX="$(xcrun --find clang++)"; fi
 echo "[*] Building libtsschecker"
-export TSS_INSTALL_DIR="/usr/local"
+export PROJECT_INSTALL_DIR="/usr/local"
 if [[ -z "$NO_CLEAN" ]]; then rm -rf cmake-build-release cmake-build-debug; fi
 if [[ "$RELEASE" == "1" ]]
 then
-  if [[ ! "$NO_CLEAN" == "1" ]]; then cmake -DCMAKE_INSTALL_PREFIX="${TSS_INSTALL_DIR}" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MAKE_PROGRAM=$(which make) -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_MESSAGE_LOG_LEVEL="WARNING" -G "CodeBlocks - Unix Makefiles" -S ./ -B cmake-build-release $@; fi
+  if [[ ! "$NO_CLEAN" == "1" ]]; then cmake -DCMAKE_INSTALL_PREFIX="${PROJECT_INSTALL_DIR}" -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_MAKE_PROGRAM=$(which make) -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_MESSAGE_LOG_LEVEL="WARNING" -G "CodeBlocks - Unix Makefiles" -S ./ -B cmake-build-release $@; fi
   make -s -C cmake-build-release clean
   make -s -C cmake-build-release
   if [[ "$?" -gt 0 ]]; then echo "[!] Failed to build libtsschecker!"; exit 1; fi
-  echo "[*] Run make -C cmake-build-release install, to install libtsschecker or obtain the the static lib at cmake-build-release/src/libtsschecker.a"
+  echo "[*] Run make -C cmake-build-release install, to install libtsschecker or obtain the static lib at cmake-build-release/$(ls cmake-build-release/ | grep libtsschecker | tr -d '\n')"
   echo "[*] Successfully built libtsschecker."
 else
-  if [[ ! "$NO_CLEAN" == "1" ]]; then cmake -DCMAKE_INSTALL_PREFIX="${TSS_INSTALL_DIR}" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=$(which make) -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_MESSAGE_LOG_LEVEL="WARNING" -G "CodeBlocks - Unix Makefiles" -S ./ -B cmake-build-debug $@ ; fi
+  if [[ ! "$NO_CLEAN" == "1" ]]; then cmake -DCMAKE_INSTALL_PREFIX="${PROJECT_INSTALL_DIR}" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MAKE_PROGRAM=$(which make) -DCMAKE_C_COMPILER="${CC}" -DCMAKE_CXX_COMPILER="${CXX}" -DCMAKE_MESSAGE_LOG_LEVEL="WARNING" -G "CodeBlocks - Unix Makefiles" -S ./ -B cmake-build-debug $@ ; fi
   make -s -C cmake-build-debug clean
   make -s -C cmake-build-debug
   if [[ "$?" -gt 0 ]]; then echo "[!] Failed to build libtsschecker!"; exit 1; fi
-  echo "[*] Run make -C cmake-build-debug install, to install libtsschecker or obtain the static lib at cmake-build-debug/src/libtsschecker.a"
+  echo "[*] Run make -C cmake-build-debug install, to install libtsschecker or obtain the static lib at cmake-build-debug/$(ls cmake-build-debug/ | grep libtsschecker | tr -d '\n')"
   echo "[*] Successfully built libtsschecker."
 fi
